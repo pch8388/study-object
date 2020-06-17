8ec6ca39ba56f6e09d138a37ebbf07ca60140dbc
pch8388 <pch838811@gmail.com>
2020-06-18 00:08:22 +0900

    refactor : 중복코드 수정 - 잘못된 예

--- src/test/java/me/study/phone/domain/PhoneTest.java
+++ src/test/java/me/study/phone/domain/PhoneTest.java
@@ -12,12 +12,12 @@ class PhoneTest {
 
 	@Test
 	public void calculateFee() {
-		Phone phone = new Phone(Money.wons(5), Duration.ofSeconds(10));
+		Phone phone = new Phone(Money.wons(5), Duration.ofSeconds(10), 0.1);
 		phone.call(new Call(LocalDateTime.of(2018, 1, 1, 12, 10, 0),
 							LocalDateTime.of(2018, 1, 1, 12, 11, 0)));
 		phone.call(new Call(LocalDateTime.of(2018, 1, 2, 12, 10, 0),
 							LocalDateTime.of(2018, 1, 2, 12, 11, 0)));
 
-		assertThat(phone.calculateFee()).isEqualTo(Money.wons(60));
+		assertThat(phone.calculateFee()).isEqualTo(Money.wons(66));
 	}
 }
