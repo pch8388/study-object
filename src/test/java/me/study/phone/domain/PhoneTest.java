023a50aaaf44c858284babb298e873aea7bc9ede
pch8388 <pch838811@gmail.com>
2020-06-19 00:25:22 +0900

    test : java util 상속의 오류

--- src/test/java/me/study/phone/domain/PhoneTest.java
+++ src/test/java/me/study/phone/domain/PhoneTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Properties;
+import java.util.Stack;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,5 +21,24 @@ class PhoneTest {
 							LocalDateTime.of(2018, 1, 2, 12, 11, 0)));
 
 		assertThat(phone.calculateFee()).isEqualTo(Money.wons(66));
+	}
+
+	@Test
+	public void utilClass() {
+		Stack<String> stack = new Stack<>();
+		stack.push("1st");
+		stack.push("2nd");
+		stack.push("3rd");
+
+		stack.add(0, "4th");
+
+		assertThat(stack.pop()).isEqualTo("3rd");
+
+		Properties properties = new Properties();
+		properties.setProperty("Bjarne Stroustrup", "C++");
+		properties.setProperty("James Gosling", "Java");
+
+		properties.put("Dennis Ritchie", 67);
+		assertThat(properties.getProperty("Dennis Ritchie")).isNull();
 	}
 }
